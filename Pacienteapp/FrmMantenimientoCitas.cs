e10c2908996a27d2a1e3b99430cba0789041bc77
JavierPDC0313 <202010620@itla.edu.do>
2021-08-21 18:24:55 -0400

    Fixed UserExists Method

--- Pacienteapp/FrmMantenimientoCitas.cs
+++ Pacienteapp/FrmMantenimientoCitas.cs
@@ -5,14 +5,34 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using BussinessLayer;
 
 namespace Pacienteapp
 {
-    public partial class FrmMantenimientoCitas : Form
+    public sealed partial class FrmMantenimientoCitas : Form
     {
-        public FrmMantenimientoCitas()
+
+        private int? id;
+
+        private bool isEdit;
+
+        private MantenimientoCitas _mantenimiento;
+
+        private FrmMantenimientoCitas()
         {
             InitializeComponent();
+        }
+
+        public static FrmMantenimientoCitas Instancia { get; set; } = new FrmMantenimientoCitas();
+
+        private void FrmMantenimientoCitas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmMantenimientoCitas_Activated(object sender, EventArgs e)
+        {
+
         }
     }
 }
