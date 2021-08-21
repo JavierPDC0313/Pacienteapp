039dda527aa0e26751182d1e1fb7d33aa1d98d1b
ÁngelRafael420 <202010085@itla.edu.do>
2021-08-20 21:32:50 -0500

    Nino's work

---
+++ Mantenimentos/FrmMedicos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Mantenimentos
+{
+    public partial class FrmMedicos : Form
+    {
+        public bool Editar = false;
+        internal object TxtIdMedicos;
+        internal object TxtCodigo;
+        internal string TxtDireccion;
+        internal string TxtCorreo;
+
+        public FrmMedicos()
+        {
+            InitializeComponent();
+        }
+
+        public string TxtCIORUC { get; internal set; }
+        public string TxtRazonSocial { get; internal set; }
+        public string TxtTelefono { get; internal set; }
+
+        internal static object MostrarMedicos()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FrmMedicos_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
