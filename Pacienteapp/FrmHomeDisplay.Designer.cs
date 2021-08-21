776c461d2b4025cc183f9de30d57872be794f857
Jorge Luis Sepúlveda Abreu <202010714@itla.edu.do>
2021-08-21 07:56:09 -0400

    Home's Interface done

--- Pacienteapp/FrmHomeDisplay.Designer.cs
+++ Pacienteapp/FrmHomeDisplay.Designer.cs
@@ -29,6 +29,7 @@ namespace Pacienteapp
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmHomeDisplay));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.btnMatenimiento_Usuario_Paciente = new System.Windows.Forms.Button();
             this.btnMatenimiento_Medico_Citas = new System.Windows.Forms.Button();
@@ -38,63 +39,82 @@ namespace Pacienteapp
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.btnMatenimiento_Usuario_Paciente, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.btnMatenimiento_Medico_Citas, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio, 0, 2);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(144, 80);
+            this.tableLayoutPanel1.Cursor = System.Windows.Forms.Cursors.No;
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(503, 253);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(425, 355);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // btnMatenimiento_Usuario_Paciente
             // 
             this.btnMatenimiento_Usuario_Paciente.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnMatenimiento_Usuario_Paciente.Location = new System.Drawing.Point(134, 21);
+            this.btnMatenimiento_Usuario_Paciente.BackColor = System.Drawing.Color.DarkGray;
+            this.btnMatenimiento_Usuario_Paciente.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMatenimiento_Usuario_Paciente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMatenimiento_Usuario_Paciente.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMatenimiento_Usuario_Paciente.Location = new System.Drawing.Point(95, 38);
             this.btnMatenimiento_Usuario_Paciente.Name = "btnMatenimiento_Usuario_Paciente";
             this.btnMatenimiento_Usuario_Paciente.Size = new System.Drawing.Size(234, 42);
             this.btnMatenimiento_Usuario_Paciente.TabIndex = 0;
             this.btnMatenimiento_Usuario_Paciente.Text = "button";
-            this.btnMatenimiento_Usuario_Paciente.UseVisualStyleBackColor = true;
+            this.btnMatenimiento_Usuario_Paciente.UseVisualStyleBackColor = false;
             // 
             // btnMatenimiento_Medico_Citas
             // 
             this.btnMatenimiento_Medico_Citas.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnMatenimiento_Medico_Citas.Location = new System.Drawing.Point(134, 105);
+            this.btnMatenimiento_Medico_Citas.BackColor = System.Drawing.Color.DarkGray;
+            this.btnMatenimiento_Medico_Citas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMatenimiento_Medico_Citas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMatenimiento_Medico_Citas.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMatenimiento_Medico_Citas.Location = new System.Drawing.Point(95, 156);
             this.btnMatenimiento_Medico_Citas.Name = "btnMatenimiento_Medico_Citas";
             this.btnMatenimiento_Medico_Citas.Size = new System.Drawing.Size(234, 42);
             this.btnMatenimiento_Medico_Citas.TabIndex = 0;
             this.btnMatenimiento_Medico_Citas.Text = "button";
-            this.btnMatenimiento_Medico_Citas.UseVisualStyleBackColor = true;
+            this.btnMatenimiento_Medico_Citas.UseVisualStyleBackColor = false;
             // 
             // btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio
             // 
             this.btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio.Location = new System.Drawing.Point(134, 189);
+            this.btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio.BackColor = System.Drawing.Color.DarkGray;
+            this.btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio.Location = new System.Drawing.Point(95, 274);
             this.btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio.Name = "btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio";
             this.btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio.Size = new System.Drawing.Size(234, 42);
             this.btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio.TabIndex = 0;
             this.btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio.Text = "button";
-            this.btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio.UseVisualStyleBackColor = true;
+            this.btnMatenimiento_PruebaLaboratorio_ResultadoLaboratorio.UseVisualStyleBackColor = false;
             // 
             // FrmHomeDisplay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(791, 423);
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(425, 355);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "FrmHomeDisplay";
-            this.Text = "FrmHomeDisplay";
+            this.Text = "Home\'s display";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmHomeDisplay_FormClosed);
             this.Load += new System.EventHandler(this.FrmHomeDisplay_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
