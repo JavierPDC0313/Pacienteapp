c149b011a4abc67be487efddf997ba0bb7ec9be8
JavierPDC0313 <202010620@itla.edu.do>
2021-08-23 03:41:07 -0400

    MantenimientoResultados Added

--- Pacienteapp/FrmResultadoLaboratorio.Designer.cs
+++ Pacienteapp/FrmResultadoLaboratorio.Designer.cs
@@ -31,18 +31,15 @@ namespace Pacienteapp
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmResultadoLaboratorio));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.btnAgregar = new System.Windows.Forms.Button();
-            this.btnEditar = new System.Windows.Forms.Button();
-            this.btnEliminar = new System.Windows.Forms.Button();
-            this.dGVPantallaListado = new System.Windows.Forms.DataGridView();
+            this.DgvPantallaListado = new System.Windows.Forms.DataGridView();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TxtBuscar = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.btnLimpiarBusqueda = new System.Windows.Forms.Button();
-            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.btnReportarResultados = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dGVPantallaListado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvPantallaListado)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
@@ -54,12 +51,9 @@ namespace Pacienteapp
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.40267F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.59733F));
-            this.tableLayoutPanel1.Controls.Add(this.dGVPantallaListado, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.DgvPantallaListado, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.btnEditar, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.btnAgregar, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.btnEliminar, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.btnReportarResultados, 1, 4);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
@@ -74,66 +68,28 @@ namespace Pacienteapp
             this.tableLayoutPanel1.Size = new System.Drawing.Size(673, 467);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // btnAgregar
+            // DgvPantallaListado
             // 
-            this.btnAgregar.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnAgregar.BackColor = System.Drawing.Color.Gainsboro;
-            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAgregar.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnAgregar.Location = new System.Drawing.Point(510, 121);
-            this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(129, 46);
-            this.btnAgregar.TabIndex = 0;
-            this.btnAgregar.Text = "Agregar";
-            this.btnAgregar.UseVisualStyleBackColor = false;
-            // 
-            // btnEditar
-            // 
-            this.btnEditar.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnEditar.BackColor = System.Drawing.Color.Gainsboro;
-            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEditar.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnEditar.Location = new System.Drawing.Point(510, 209);
-            this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(129, 46);
-            this.btnEditar.TabIndex = 0;
-            this.btnEditar.Text = "Editar";
-            this.btnEditar.UseVisualStyleBackColor = false;
-            // 
-            // btnEliminar
-            // 
-            this.btnEliminar.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnEliminar.BackColor = System.Drawing.Color.Gainsboro;
-            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEliminar.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnEliminar.Location = new System.Drawing.Point(510, 297);
-            this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(129, 46);
-            this.btnEliminar.TabIndex = 0;
-            this.btnEliminar.Text = "Eliminar";
-            this.btnEliminar.UseVisualStyleBackColor = false;
-            // 
-            // dGVPantallaListado
-            // 
-            this.dGVPantallaListado.AllowUserToAddRows = false;
-            this.dGVPantallaListado.AllowUserToDeleteRows = false;
-            this.dGVPantallaListado.BackgroundColor = System.Drawing.Color.Gainsboro;
-            this.dGVPantallaListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dGVPantallaListado.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dGVPantallaListado.Location = new System.Drawing.Point(15, 103);
-            this.dGVPantallaListado.Name = "dGVPantallaListado";
-            this.dGVPantallaListado.ReadOnly = true;
-            this.tableLayoutPanel1.SetRowSpan(this.dGVPantallaListado, 4);
-            this.dGVPantallaListado.RowTemplate.Height = 25;
-            this.dGVPantallaListado.Size = new System.Drawing.Size(470, 349);
-            this.dGVPantallaListado.TabIndex = 1;
+            this.DgvPantallaListado.AllowUserToAddRows = false;
+            this.DgvPantallaListado.AllowUserToDeleteRows = false;
+            this.DgvPantallaListado.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.DgvPantallaListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvPantallaListado.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DgvPantallaListado.Location = new System.Drawing.Point(15, 103);
+            this.DgvPantallaListado.Name = "DgvPantallaListado";
+            this.DgvPantallaListado.ReadOnly = true;
+            this.tableLayoutPanel1.SetRowSpan(this.DgvPantallaListado, 4);
+            this.DgvPantallaListado.RowTemplate.Height = 25;
+            this.DgvPantallaListado.Size = new System.Drawing.Size(470, 349);
+            this.DgvPantallaListado.TabIndex = 1;
+            this.DgvPantallaListado.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvPantallaListado_CellClick);
             // 
             // tableLayoutPanel2
             // 
             this.tableLayoutPanel2.ColumnCount = 2;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.TxtBuscar, 0, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(15, 15);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
@@ -143,38 +99,16 @@ namespace Pacienteapp
             this.tableLayoutPanel2.Size = new System.Drawing.Size(470, 82);
             this.tableLayoutPanel2.TabIndex = 2;
             // 
-            // textBox1
+            // TxtBuscar
             // 
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.tableLayoutPanel2.SetColumnSpan(this.textBox1, 2);
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.textBox1.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(3, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(464, 26);
-            this.textBox1.TabIndex = 0;
-            // 
-            // btnBuscar
-            // 
-            this.btnBuscar.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.btnBuscar.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnBuscar.Location = new System.Drawing.Point(3, 3);
-            this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(161, 35);
-            this.btnBuscar.TabIndex = 1;
-            this.btnBuscar.Text = "Buscar";
-            this.btnBuscar.UseVisualStyleBackColor = true;
-            // 
-            // btnLimpiarBusqueda
-            // 
-            this.btnLimpiarBusqueda.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnLimpiarBusqueda.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnLimpiarBusqueda.Location = new System.Drawing.Point(3, 44);
-            this.btnLimpiarBusqueda.Name = "btnLimpiarBusqueda";
-            this.btnLimpiarBusqueda.Size = new System.Drawing.Size(161, 34);
-            this.btnLimpiarBusqueda.TabIndex = 1;
-            this.btnLimpiarBusqueda.Text = "Limpiar";
-            this.btnLimpiarBusqueda.UseVisualStyleBackColor = true;
+            this.TxtBuscar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tableLayoutPanel2.SetColumnSpan(this.TxtBuscar, 2);
+            this.TxtBuscar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.TxtBuscar.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TxtBuscar.Location = new System.Drawing.Point(3, 12);
+            this.TxtBuscar.Name = "TxtBuscar";
+            this.TxtBuscar.Size = new System.Drawing.Size(464, 26);
+            this.TxtBuscar.TabIndex = 0;
             // 
             // tableLayoutPanel3
             // 
@@ -191,6 +125,30 @@ namespace Pacienteapp
             this.tableLayoutPanel3.Size = new System.Drawing.Size(167, 82);
             this.tableLayoutPanel3.TabIndex = 3;
             // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnBuscar.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnBuscar.Location = new System.Drawing.Point(3, 3);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(161, 35);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnLimpiarBusqueda
+            // 
+            this.btnLimpiarBusqueda.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLimpiarBusqueda.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLimpiarBusqueda.Location = new System.Drawing.Point(3, 44);
+            this.btnLimpiarBusqueda.Name = "btnLimpiarBusqueda";
+            this.btnLimpiarBusqueda.Size = new System.Drawing.Size(161, 34);
+            this.btnLimpiarBusqueda.TabIndex = 1;
+            this.btnLimpiarBusqueda.Text = "Limpiar";
+            this.btnLimpiarBusqueda.UseVisualStyleBackColor = true;
+            this.btnLimpiarBusqueda.Click += new System.EventHandler(this.btnLimpiarBusqueda_Click);
+            // 
             // btnReportarResultados
             // 
             this.btnReportarResultados.Anchor = System.Windows.Forms.AnchorStyles.None;
@@ -203,6 +161,8 @@ namespace Pacienteapp
             this.btnReportarResultados.TabIndex = 0;
             this.btnReportarResultados.Text = "Reportar";
             this.btnReportarResultados.UseVisualStyleBackColor = false;
+            this.btnReportarResultados.Visible = false;
+            this.btnReportarResultados.Click += new System.EventHandler(this.btnReportarResultados_Click);
             // 
             // FrmResultadoLaboratorio
             // 
@@ -214,8 +174,10 @@ namespace Pacienteapp
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "FrmResultadoLaboratorio";
             this.Text = "Resultados de laboratorio";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmResultadoLaboratorio_FormClosing);
+            this.Load += new System.EventHandler(this.FrmResultadoLaboratorio_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dGVPantallaListado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvPantallaListado)).EndInit();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
             this.tableLayoutPanel3.ResumeLayout(false);
@@ -227,12 +189,9 @@ namespace Pacienteapp
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.DataGridView dGVPantallaListado;
+        private System.Windows.Forms.DataGridView DgvPantallaListado;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button btnEliminar;
-        private System.Windows.Forms.Button btnEditar;
-        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox TxtBuscar;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
         private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.Button btnLimpiarBusqueda;
